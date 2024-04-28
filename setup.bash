59becda30f514bb498f22791bef6d3887287b31b
Nbobito <nbobito12@gmail.com>
2024-04-27 23:04:12 -0600

    I think it kinda works now

--- setup.bash
+++ setup.bash
@@ -1,1 +1,6 @@
-cp default_setitngs.yaml settings.yaml
+cp default_settings.yaml settings.yaml
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+neofetch
+sudo pacman -S lolcat
